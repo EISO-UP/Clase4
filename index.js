67e51c090f7d3cf151c27404148cf95388b0f9bc
Enrique <0208738@up.edu.mx>
2022-10-21 19:55:23 -0500

    Clase 21/10/22

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const { router } = require('./routes')
 const app = express();
 const apiPort = process.env.API_PORT || 3003;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', router);
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
